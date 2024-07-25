602689221af41a4c187b9ee4ff6e693677f9ec3e
ABDULLAH <141825353+RHETORICALWORDSMITH@users.noreply.github.com>
2024-07-25 11:29:32 +0500

    Update CartCards.jsx

--- Frontend/src/components/CartCards.jsx
+++ Frontend/src/components/CartCards.jsx
@@ -47,8 +47,8 @@ const CartCards = () => {
 
   return (
     <>
-      <span className="text-4xl text-yellow-500 w-auto flex justify-center items-center mb-5">{`Total Bill: ${totalBill}$ 💰`}</span>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center mb-12">
+      <span className="text-4xl text-yellow-500 mb-5">{`Total Bill: ${totalBill}$ 💰`}</span>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-12 gap-9">
         {boughtItems.map((boughtItem) => {
           if (boughtItem.noItem > 0) {
             return (
@@ -61,85 +61,87 @@ const CartCards = () => {
                       alt="movie img"
                     />
                   </figure>
-                  <div className="card-body ">
-                    <h2 className="card-title">
+                  <div className="card-body min-h-52">
+                    <h2 className="card-title h-9">
                       {boughtItem.name}
                       <div className="badge badge-secondary">
                         {boughtItem.category}
                       </div>
                     </h2>
-                    <p>{boughtItem.title}</p>
-                    <div className="flex flex-col pb-14">
-                      <div className="card-actions flex justify-between items-center">
-                        <div className="px-3 py-1 border rounded-full">
-                          {`${boughtItem.price * boughtItem.noItem}$`}
+                    <div className="flex flex-col gap-3 mt-4">
+                      <p className="min-h-20">{boughtItem.title}</p>
+                      <div className="flex flex-col pb-14">
+                        <div className="card-actions flex justify-between items-center">
+                          <div className="px-3 py-1 border rounded-full">
+                            {`${boughtItem.price * boughtItem.noItem}$`}
+                          </div>
+                          <div
+                            className="cursor-pointer px-2 py-1 border rounded-full hover:bg-pink-500 hover:border-none hover:text-black hover:font-bold"
+                            onClick={handleVisibility}
+                          >
+                            Modify
+                          </div>
                         </div>
                         <div
-                          className="cursor-pointer px-2 py-1 border rounded-full hover:bg-pink-500 hover:border-none hover:text-black hover:font-bold"
-                          onClick={handleVisibility}
+                          className={`justify-between absolute gap-16 bottom-5 mt-5 transition-all ease-in-out duration-200 ${
+                            visibility ? "flex" : "hidden"
+                          }`}
                         >
-                          Modify
-                        </div>
-                      </div>
-                      <div
-                        className={`justify-between absolute gap-16 bottom-5 mt-5 transition-all ease-in-out duration-200 ${
-                          visibility ? "flex" : "hidden"
-                        }`}
-                      >
-                        <div className="flex items-center gap-5">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-8"
-                            viewBox="0 -960 960 960"
-                            fill="#EAC452"
+                          <div className="flex items-center gap-5">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="w-8"
+                              viewBox="0 -960 960 960"
+                              fill="#EAC452"
+                              onClick={() =>
+                                dispatch(
+                                  updateDecrement({
+                                    noItem: 1,
+                                    id: boughtItem.id,
+                                    totalItems: boughtItem.noItem,
+                                  })
+                                )
+                              }
+                            >
+                              <path d="M200-446.67v-66.66h560v66.66H200Z" />
+                            </svg>
+
+                            <p className="text-2xl dark:text-white text-black">
+                              {boughtItem.noItem}
+                            </p>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 -960 960 960"
+                              className="w-8"
+                              fill="#EAC452"
+                              onClick={() =>
+                                dispatch(
+                                  updateIncrement({
+                                    noItem: 1,
+                                    id: boughtItem.id,
+                                  })
+                                )
+                              }
+                            >
+                              <path d="M446.67-446.67H200v-66.66h246.67V-760h66.66v246.67H760v66.66H513.33V-200h-66.66v-246.67Z" />
+                            </svg>
+                          </div>
+
+                          <button
+                            className="px-4 py-1 rounded-md text-white bg-pink-500 dark:bg-pink-700 hover:text-black hover:bg-pink-700 duration-200"
                             onClick={() =>
                               dispatch(
                                 updateDecrement({
-                                  noItem: 1,
+                                  noItem: boughtItem.noItem,
                                   id: boughtItem.id,
                                   totalItems: boughtItem.noItem,
                                 })
                               )
                             }
                           >
-                            <path d="M200-446.67v-66.66h560v66.66H200Z" />
-                          </svg>
-
-                          <p className="text-2xl dark:text-white text-black">
-                            {boughtItem.noItem}
-                          </p>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 -960 960 960"
-                            className="w-8"
-                            fill="#EAC452"
-                            onClick={() =>
-                              dispatch(
-                                updateIncrement({
-                                  noItem: 1,
-                                  id: boughtItem.id,
-                                })
-                              )
-                            }
-                          >
-                            <path d="M446.67-446.67H200v-66.66h246.67V-760h66.66v246.67H760v66.66H513.33V-200h-66.66v-246.67Z" />
-                          </svg>
+                            Cancel
+                          </button>
                         </div>
-
-                        <button
-                          className="px-4 py-1 rounded-md text-white bg-pink-500 dark:bg-pink-700 hover:text-black hover:bg-pink-700 duration-200"
-                          onClick={() =>
-                            dispatch(
-                              updateDecrement({
-                                noItem: boughtItem.noItem,
-                                id: boughtItem.id,
-                                totalItems: boughtItem.noItem,
-                              })
-                            )
-                          }
-                        >
-                          Cancel
-                        </button>
                       </div>
                     </div>
                   </div>
